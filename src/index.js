5fb3eac11152ba0f84a3e228307a79ad1bf93a56
phamtuankiet61 <3000351179.ct@hue.edu.vn>
2023-01-02 13:52:54 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const sass = require('node-sass');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);
